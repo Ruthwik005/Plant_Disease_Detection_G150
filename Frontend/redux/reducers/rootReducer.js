161b55ed791bfd32b9980e8dc5a5db731d9df12e
Chamala Ruthwik <140952355+Ruthwik005@users.noreply.github.com>
2024-12-23 20:25:13 +0530

    Delete Frontend/redux directory

--- Frontend/redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import someReducer from './someReducer.js';  // Replace with your actual reducer
-
-const rootReducer = combineReducers({
-  someState: someReducer,  // Add other reducers if needed
-});
-
-export default rootReducer;
